dd1253a959cfb65932afed3f7612d2947a36df84
Samantha Ball <samball999@gmail.com>
2021-03-29 14:10:34 +0200

    created methods to calculate entropy and information gain

--- id3.py
+++ id3.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 #--- Implementing ID3 Decision Tree ---#
 
 # Iterative Dichotomiser 3 - iteratively dichotomizes(divides) features into two or more groups at each step
@@ -19,19 +21,101 @@
 # Entropy(S) = - ∑ pᵢ * log₂(pᵢ) ; i = 1 to n
 
 
-# NB are we calculating for one feature or all features- can we efficiently calculate for all features at once? - vectorized implementation?
+
 # test this function in isolation
 # our classes are A and B
-def entropy(data, index):
+# NB it is entropy of target -> True and False
+def entropy(data):
     sum = 0
-    feature = data[index] # NB try to do for all features
-    classes = np.unique(feature)
-    print(classes)
+    target = data["Target"] # target us last column - how to store our data? array of columns? but then how do we index rows?
+    classes = np.unique(target)
+    #print(classes)
     # iterate over classes (will be 2 in our case)
-    for i in range(len(classes)):
-        prob_i = len(data[feature==i])/len(feature) # probability of class ‘i’ = “number of rows with class i in the target column” to the “total number of rows” in the dataset
+    for i in classes:
+        #print(len(data[data["Target"]==i]))
+        #print(len(target))
+        prob_i = len(data[data["Target"]==i])/len(target) # probability of class ‘i’ = “number of rows with class i in the target column” to the “total number of rows” in the dataset
         log_2_p_i  = np.log(prob_i) / np.log(2) # check this
         sum += prob_i*log_2_p_i
 
+    #print(sum)
     entropy = sum*-1
+    #print(entropy)
     return entropy
+
+
+
+# information gain for specified feature column
+# IG(S, A) = Entropy(S) - ∑((|Sᵥ| / |S|) * Entropy(Sᵥ))
+# where Sᵥ is the set of rows in S for which the feature column A has value v, |Sᵥ| is the number of rows in Sᵥ and likewise |S| is the number of rows in S.
+# how does this differ for each feature??
+def get_information_gain(data, feature):
+    total_entropy = entropy(data) # entropy of target
+    classes = np.unique(data[feature])
+    #print(classes)
+    summation = 0
+    for v in classes:
+        set_v = data[data[feature]==v]# set of rows in S for which the feature column A has value v
+        entropy_set_v = entropy(set_v)
+        scaling_factor =  len(set_v)/len(data[feature])
+        #print(len(set_v))
+        #print(len(data[feature]))
+        #print(scaling_factor)
+        summation += scaling_factor*entropy_set_v
+    
+    #print(summation)
+    info_gain = total_entropy - summation
+    return info_gain
+
+
+
+# how to check if correct info_gain is being calculated??
+
+
+
+
+# 1. Calculate the Information Gain of each feature.
+# 2. Considering that all rows don’t belong to the same class, split the dataset S into subsets using the feature for which the Information Gain is maximum.
+# 3. Make a decision tree node using the feature with the maximum Information gain.
+# 4. If all rows belong to the same class, make the current node as a leaf node with the class as its label.
+# 5. Repeat for the remaining features until we run out of all features, or the decision tree has all leaf nodes.
+def id_3(data):
+    information_gains = [] # use dictionary instead?
+    # iterate over features
+    no_features = data.shape[1] - 2 # subtract target row and index row
+    print(no_features)
+    for i in range(1, no_features+1):
+        info_gain = get_information_gain(data, i) # or should i be the feature column?? how best to index everything?
+        information_gains.append(info_gain)
+    
+    # split dataset into subsets using the feature for which info gain is max
+    # info gain has to do with how it is splitting the target True and Falses! NB to understand for theory part of report
+    max_gain =  np.max(information_gains)
+    print(max_gain)
+    max_gain_feature = np.argmax(information_gains)
+    print(max_gain_feature)
+    print(len(data[data[4]=='A']))
+
+
+    min_gain =  np.min(information_gains)
+    print(min_gain)
+    min_gain_feature = np.argmin(information_gains)
+    print(min_gain_feature)
+    print(len(data[data[9]=='A']))
+
+    classes = np.unique(data[max_gain_feature])
+    
+    subset_a = data[data[max_gain_feature] == classes[0]] # all rows where feature =  A
+    subset_b =  data[data[max_gain_feature] == classes[1]]  # all rows where feature =  B
+    print(subset_a.head())
+
+    # make a decision tree node using the feature with the maximum Information gain
+
+
+    # if all rows belong to the same class, make the current node as a leaf node with the class as its label
+
+
+    # repeat for the remaining features until we run out of all features, or the decision tree has all leaf nodes
+
+
+
--- run_id3.py
+++ run_id3.py
@@ -1,11 +1,49 @@
 # main class
 
+import pandas as pd
+from id3 import id_3 # better way to do this?
+
+
+# how best to store these features and target?
+def read_text_file(filename):
+    data_file = open(filename,"r")
+    data = []
+    for line in data_file.readlines():
+        data.append(line)
+    print(len(data)) # 5000 training examples
+    print(len(data[0])) # 108 -> 100 features, letters in True, spaces and \n -> encode true and false and 1 and 0?
+    print(data[0])
+    data_file.close()
+    return data
+
+
+# alternative - use pandas??
+def read_text_file_alt(filename):
+    print("\nReading in training dataset...")
+    data = pd.read_csv(filename, header = None) 
+    # first separate features and targets
+    features_and_target = data[0].str.split(pat=" ", expand=True)
+    data["Features"] = features_and_target[0]
+    data["Target"] = features_and_target[1]
+    # split features into their own columns
+    split_features = data["Features"].str.split(pat="", expand=True)
+    for i in range(split_features.shape[1]-1):
+        data[i] = split_features[i] # does this index the column??
+
+    data.drop(columns =["Features"], inplace = True) # drop old features column
+    print(data.head())
+    return data
 
 
 
 
 def main():
-    print("Hello World!")
+    print("Feature Optimization for ID3")
+    data = read_text_file_alt("./Data/Training_Data.txt")
+    #entropy(data)
+    #info_gain_A = get_information_gain(data, 95) # must it be string or number
+    #print(info_gain_A)
+    id_3(data)
 
 if __name__ == "__main__":
     main()
